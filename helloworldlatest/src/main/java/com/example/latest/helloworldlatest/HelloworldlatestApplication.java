45dc6687fb73bdaf836a3517d31cc3634341a186
91908 <raguram6732@gmail.com>
2023-12-23 10:15:40 +0530

    Initial commit

---
+++ helloworldlatest/src/main/java/com/example/latest/helloworldlatest/HelloworldlatestApplication.java
@@ -0,0 +1,13 @@
+package com.example.latest.helloworldlatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldlatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldlatestApplication.class, args);
+	}
+
+}
